5d453d788ceeaf1bdfd0182a0f7fa130737f4d8e
PriyankaMj <pm01.03.14@gmail.com>
2020-07-19 18:57:17 +0530

    Add files via upload

---
+++ Assets/Scripts/ParamField.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+    public class ParamField : MonoBehaviour, ISelectHandler
+    {
+        public GameObject Value;
+        public void OnSelect(BaseEventData eventData)//Where text selected it send the text field information to then Master(Keyboard)
+        {
+            Master cantrolMaster = GameObject.Find("Master").GetComponent<Master>();
+            cantrolMaster.SetIt(Value);
+        }
+    }
